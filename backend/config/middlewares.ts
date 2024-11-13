823f31568259c46331aa7ef218c7dc50e4770c08
Aman Yadav <itsamanyadav18@gmail.com>
2024-11-13 11:49:44 +0530

    cors fix 1

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -5,7 +5,7 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://13.235.104.162:8080'], // Allow requests from your frontend
+      origin: ["http://13.235.104.162:3000", "http://13.235.104.162:8080"], // Allow requests from your frontend
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
     },
