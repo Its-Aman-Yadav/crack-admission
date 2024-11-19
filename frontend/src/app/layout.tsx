988e5b316970e6e034d2a8964a6757f0a0c19e0a
Aman Yadav <itsamanyadav18@gmail.com>
2024-11-20 02:44:55 +0530

    Google Tag Manager Added for Analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import Link from "next/link";
 import "./globals.css";
-import Navbar from "@/components/Navbar"; 
-import Footer from "@/components/Footer"; 
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 // Custom font configurations
 const geistSans = localFont({
@@ -35,14 +34,41 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N2MK8ZFX');
+            `,
+          }}
+        />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <meta name="description" content="Achieve your MBA dreams with Crack Admission's personalized consulting services. We specialize in comprehensive application strategies for top business schools like Stanford, INSEAD, Wharton, and more." />
-        <meta name="keywords" content="MBA consulting, business school applications, MBA essays, GMAT, GRE, Stanford, INSEAD, Wharton, Harvard, MBA interview prep, scholarship essays" />
+        <meta
+          name="description"
+          content="Achieve your MBA dreams with Crack Admission's personalized consulting services. We specialize in comprehensive application strategies for top business schools like Stanford, INSEAD, Wharton, and more."
+        />
+        <meta
+          name="keywords"
+          content="MBA consulting, business school applications, MBA essays, GMAT, GRE, Stanford, INSEAD, Wharton, Harvard, MBA interview prep, scholarship essays"
+        />
         <title>Crack Admission: MBA Consulting Experts</title>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager Noscript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N2MK8ZFX"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Navbar /> {/* Navbar included here */}
         {children}
         <Footer />
