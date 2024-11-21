a88873e4683ef3cc613cd92efaa6d1f11e945494
Aman Yadav <itsamanyadav18@gmail.com>
2024-11-22 00:26:06 +0530

    configured server.ts in backend

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -2,8 +2,4 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: 'https://crackadmission.com', // Optional for production
-  https: {
-    key: env('SSL_KEY_PATH'),
-    cert: env('SSL_CERT_PATH'),
-  },
 });
