828f64392d41cf283198c8ac249c7e4e5fbe8a62
Aman Yadav <itsamanyadav18@gmail.com>
2024-11-26 03:39:55 +0530

    updates next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'images.pexels.com'], // Add multiple domains in a single array
+    domains: ['localhost', 'images.pexels.com', 'www.crackadmission.com'], // Add multiple domains in a single array
   },
 };
 
