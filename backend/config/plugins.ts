7242d03527ab280723280a97779b03337c741ec9
Aman Yadav <itsamanyadav18@gmail.com>
2024-11-26 04:28:53 +0530

    added plugins and cipher security

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,1 +1,15 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'local',
+        providerOptions: {
+          sizeLimit: 1000000, // Size limit in bytes
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+  });
+  
