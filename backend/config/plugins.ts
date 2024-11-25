5ca474531e7a849487b858ae8f9e25dcd8dea74e
Aman Yadav <itsamanyadav18@gmail.com>
2024-11-26 05:01:59 +0530

    removes plugin

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,15 +1,15 @@
-module.exports = ({ env }) => ({
-    upload: {
-      config: {
-        provider: 'local',
-        providerOptions: {
-          sizeLimit: 1000000, // Size limit in bytes
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
-      },
-    },
-  });
+// module.exports = ({ env }) => ({
+//     upload: {
+//       config: {
+//         provider: 'local',
+//         providerOptions: {
+//           sizeLimit: 1000000, // Size limit in bytes
+//         },
+//         actionOptions: {
+//           upload: {},
+//           delete: {},
+//         },
+//       },
+//     },
+//   });
   
